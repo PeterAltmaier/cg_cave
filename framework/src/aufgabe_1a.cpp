3439d428e5503a77e6d79aa8dde61805be4566d0
DanielH <daniel.huschina@uni-bonn.de>
2020-06-16 13:29:41 +0200

    Aufgabe 1 mit Kamera

--- framework/src/aufgabe_1a.cpp
+++ framework/src/aufgabe_1a.cpp
@@ -1,9 +1,18 @@
+#include <chrono>
+#include <glm/gtx/transform.hpp>
 #include "common.hpp"
 #include "shader.hpp"
 #include "buffer.hpp"
+#include <camera.hpp>
+#include "mesh.hpp"
+
+glm::mat4 proj_matrix;
 
 const int WINDOW_WIDTH =  800;
 const int WINDOW_HEIGHT = 800;
+const float FOV = 45.f;
+const float NEAR_VALUE = 0.1f;
+const float FAR_VALUE = 100.f;
 
 // called whenever the window gets resized
 void
@@ -13,6 +22,11 @@ int
 main(int, char* argv[]) {
     GLFWwindow* window = initOpenGL(WINDOW_WIDTH, WINDOW_HEIGHT, argv[0]);
     glfwSetFramebufferSizeCallback(window, resizeCallback);
+    glm::vec3 pos = glm::vec3(0.0, 0.0, 0.0);
+    glm::mat4 model = glm::translate(model, pos);
+    proj_matrix = glm::perspective(FOV, 1.f, NEAR_VALUE, FAR_VALUE);
+    camera cam(window);
+
 
     // load and compile shaders and link program
     unsigned int vertexShader = compileShader("uebung_1b.vert", GL_VERTEX_SHADER);
@@ -24,15 +38,21 @@ main(int, char* argv[]) {
 
     // vertex data
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.5f,  0.5f, 0.0f,
-         -0.5f,  0.5f, 0.0f
+         0.f, 0.f, 0.0f,
+         2.f, 0.f, 0.0f,
+         0.f, 2.f, 0.0f
     };
 
     unsigned int indices[] = {
         0, 1, 2
     };
+
+    glUseProgram(shaderProgram);
+    int model_mat_loc = glGetUniformLocation(shaderProgram, "model");
+    int view_mat_loc = glGetUniformLocation(shaderProgram, "view");
+    int proj_mat_loc = glGetUniformLocation(shaderProgram, "projection");
+
+    glEnable(GL_DEPTH_TEST);
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -48,17 +68,26 @@ main(int, char* argv[]) {
 
     // rendering loop
     while (glfwWindowShouldClose(window) == false) {
-        // set background color...
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         // and fill screen with it (therefore clearing the window)
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // render something...
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*) 0);
 
-        // swap buffers == show rendered content
+        glm::mat4 view_matrix = cam.view_matrix();
+        glUniformMatrix4fv(view_mat_loc, 1, GL_FALSE, &view_matrix[0][0]);
+        glUniformMatrix4fv(proj_mat_loc, 1, GL_FALSE, &proj_matrix[0][0]);
+        glUniformMatrix4fv(model_mat_loc, 1, GL_FALSE, &model[0][0]);
+
+
+
+        // render plane
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)0);
+
+
+
+
         glfwSwapBuffers(window);
         // process window events
         glfwPollEvents();
